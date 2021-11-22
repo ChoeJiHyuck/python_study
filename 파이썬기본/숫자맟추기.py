0bb4eb756b03febcd7a3605bcb07c10eb3a01d55
ChoeJiHyuck <93892321+ChoeJiHyuck@users.noreply.github.com>
2021-11-22 16:06:11 +0900

    2021/11/22

--- 파이썬기본/숫자맟추기.py
+++ 파이썬기본/숫자맟추기.py
@@ -9,7 +9,7 @@ name = input()
 print(f"안녕하세요 {name}님 1에서 20까지 숫자중 하나를 생각합니다.")
 secretNumber = random.randint(1, 20)  # 랜덤으로 숫자 하나 생성
 
-for count in range(1, 7):  # 6번의 맞출기회
+for count in range(1, 6):  # 6번의 맞출기회
     guess = int(input("맞춰보세요 \n"))
     # 괄호(  ) 안에 알맞는 내용을 작성하여 완성하세요 👇
     if (secretNumber > guess):
@@ -23,4 +23,4 @@ for count in range(1, 7):  # 6번의 맞출기회
 if guess == secretNumber:
     print(f'잘 맞췄어요 {name}님 {count} 번만에 맞췄어요 !')
 else:
-    print(f'틀렸네요. 정답은 {secretNumber} 입니다.')
+    print(f'틀렸습니다. 정답은 {secretNumber} 입니다.')
